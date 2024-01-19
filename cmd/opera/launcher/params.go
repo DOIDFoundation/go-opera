515ce8be2a87902a7badcbc06f623747c33ca070
ac.doid <>
2024-01-19 19:36:41 +0800

    fix mainnet bootnodes

--- cmd/opera/launcher/params.go
+++ cmd/opera/launcher/params.go
@@ -6,7 +6,7 @@ import (
 
 var (
 	Bootnodes = map[string][]string{
-		"main": {
+		"doid": {
 			"enode://5208615fbfc2b38a49fc6fcf773c500c212637bda220b885e28d1eadeab0c4ba3df6036268117bf32b84c6203aa976e349641b291c5d58295b2cf3721c5c0f53@13.234.216.14:5050",
 			"enode://574617a92ab3341f8a09fa07e0ccdb0e1cf1af498b7ae9721a3e122ee5d4fa8c5b749972a0f15a10b165abc8c282c35e821f68eb9e6bb59abbba91604de7cb64@35.237.88.8:5051",
 		},
